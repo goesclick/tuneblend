f056116eb2bf2336130f778eb75f0613b44d2734
Patrick Schreiber <ptos382@gmail.com>
2020-03-19 05:21:19 -0500

    Fleshed out build tools

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,12 @@
+/*
+ *   Copyright (c) 2020
+ *   All rights reserved.
+ */
+
 const path = require('path');
 module.exports = {
   mode: 'development',
-  entry: path.join(__dirname, 'src', 'index'),
+  entry: path.join(__dirname, 'src', 'index.ts'),
   watch: true,
   output: {
     path: path.join(__dirname, 'dist'),
@@ -9,29 +14,42 @@ module.exports = {
     filename: "bundle.js",
     chunkFilename: '[name].js'
   },
-  module: {
-    rules: [{
-      test: /.jsx?$/,
-      include: [
-        path.resolve(__dirname, 'src')
-      ],
-      exclude: [
-        path.resolve(__dirname, 'node_modules')
-      ],
-      loader: 'babel-loader',
-      query: {
-        presets: [
-          ["@babel/env", {
-            "targets": {
-              "browsers": "last 2 chrome versions"
-            }
-          }]
-        ]
-      }
-    }]
-  },
   resolve: {
-    extensions: ['.json', '.js', '.jsx']
+    // Add `.ts` and `.tsx` as a resolvable extension.
+    extensions: [".ts", ".tsx", ".js", ".json", ".jsx"]
+  },
+  module: {
+    rules: [
+
+        // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+      {
+        loader: "ts-loader",
+
+        test: /.tsx?$/,
+        include: [
+          path.resolve(__dirname, 'src')
+        ],
+      },
+      {
+        test: /.jsx?$/,
+        include: [
+          path.resolve(__dirname, 'test')
+        ],
+        exclude: [
+          path.resolve(__dirname, 'node_modules')
+        ],
+        loader: 'babel-loader',
+        query: {
+          presets: [
+            ["@babel/env", {
+              "targets": {
+                "browsers": "last 2 chrome versions"
+              }
+            }]
+          ]
+        }
+      }
+    ]
   },
   devtool: 'source-map',
   devServer: {
